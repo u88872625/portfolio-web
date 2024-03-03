1b9902796d860cdb39864a7e27392ade335d679d
u88872625 <u88872625@gmail.com>
2024-03-03 15:31:52 +0800

    feat:Navbar design

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          MING CHUAN YEH
+        </motion.span>
+
+        <a href="#">
+          <img src="/github-mark-white.svg" alt="github.svg" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
