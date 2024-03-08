5e24036322adbbec886c17d9cd803d378a0c4830
u88872625 <u88872625@gmail.com>
2024-03-08 15:58:43 +0800

    fix:sidebar animation

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,27 +4,27 @@ import Links from "./links/Links";
 import ToggleButton from "./toggleButton/ToggleButton";
 import { useState } from "react";
 
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(25px at 48px 45px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
-
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        type: "spring",
-        stiffness: 20,
-      },
-    },
-    closed: {
-      clipPath: "circle(25px at 48px 45px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     <motion.div className="sidebar" animate={open ? "open" : "closed"}>
